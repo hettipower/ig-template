dedd91fade95c007e4326d9567df1aa097d0770f
TharinduH <hettipower@gamil.com>
2023-05-13 23:26:06 +0530

    update

--- index.php
+++ index.php
@@ -30,6 +30,15 @@
                         <div class="content">
                             <h1>Delivering <span>complete</span> marketing solutions</h1>
                             <p>Established in 2004 as one of the first UK SEO companies for Google, iCONQUER provide complete internet marketing solutions. If you require an online marketing service, we have the ingredients that will add value to your business. Contact us for a no obligation consultation.</p>
+
+                            <ul>
+                                <li>5* reputation</li>
+                                <li>Over 20 years experience</li>
+                                <li>Dedicated account managers</li>
+                                <li>Monthly reports and monitoring</li>
+                                <li>Certified Google Partners</li>
+                                <li>Promotions that deliver results</li>
+                            </ul>
                         </div>
                         <div class="image">
                             <img src="./images/Weblinx_Logo(1) 1.png" alt="">
@@ -41,6 +50,15 @@
                         <div class="content">
                             <h1>Delivering <span>complete</span> marketing solutions</h1>
                             <p>Established in 2004 as one of the first UK SEO companies for Google, iCONQUER provide complete internet marketing solutions. If you require an online marketing service, we have the ingredients that will add value to your business. Contact us for a no obligation consultation.</p>
+
+                            <ul>
+                                <li>5* reputation</li>
+                                <li>Over 20 years experience</li>
+                                <li>Dedicated account managers</li>
+                                <li>Monthly reports and monitoring</li>
+                                <li>Certified Google Partners</li>
+                                <li>Promotions that deliver results</li>
+                            </ul>
                         </div>
                         <div class="image">
                             <img src="./images/Weblinx_Logo(1) 1.png" alt="">
